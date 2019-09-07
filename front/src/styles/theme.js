fca3c513f0e574675e9cc31efe528335a669a190
Bernardo Chagas <bernardochagas@usp.br>
2019-09-07 13:08:06 -0300

    Cria tema do material

---
+++ front/src/styles/theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export default createMuiTheme({
+  palette: {
+    primary: {
+      main: '#11C3CC',
+      dark: '#0F8282',
+    },
+    secondary: {
+      main: '#FBB03B',
+      dark: '#CC8A33',
+    }
+  }
+});
