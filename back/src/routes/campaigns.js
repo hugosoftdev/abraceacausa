1e1b701a44abd8e55eeff09b0701b3fd026bb864
Jumc <ju_jumc@hotmail.com>
2019-09-07 17:10:31 -0300

    Mais detalhes na campanha

--- back/src/routes/campaigns.js
+++ back/src/routes/campaigns.js
@@ -15,19 +15,25 @@ router.get('/', (req, res) => {
 router.get('/entities', (req, res) => {
     const campaign_id = req.body['campaign_id'];
     const db = connection.db("test");
-    db.collection("entity_campaign").find({campaign_id}).toArray()
-    .then(items => {
-      let ids = items.map(item => item['entity_id']);
-      db.collection("entities").find({
-        id: { $in: ids }
-      }).toArray()
-      .then(entities => {
-        var response = entities.map(entity => {
-          var needs = items.filter(a => a['entity_id'] === entity['id'])[0]['needs'];
-          entity['needs'] = needs;
+    db.collection("campaigns").findOne({id:campaign_id})
+    .then(campaign => {
+      db.collection("entity_campaign").find({campaign_id}).toArray()
+      .then(items => {
+        let ids = items.map(item => item['entity_id']);
+        db.collection("entities").find({
+          id: { $in: ids }
+        }).toArray()
+        .then(entities => {
+          var response = entities.map(entity => {
+            var needs = items.filter(a => a['entity_id'] === entity['id'])[0]['needs'];
+            entity['needs'] = needs;
+            return entity;
+          })
+          campaign['entities'] = response
+          res.send(campaign);
         })
-        res.send(entities);
       })
+      .catch(err => console.log(err));
     })
     .catch(err => console.log(err));
 });
